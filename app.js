74f884e0451494826e1ab79aa4bebeb8abc208f2
Diane Phan <diane.phan@AVOIDLONGDETECT.workdayinternal.com>
2019-07-06 13:54:33 -0700

    deleted env

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mountRoutes(app)
 
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () => console.log('Listening on port ...'));       
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));       
 
 module.export = server 
 
