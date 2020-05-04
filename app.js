afec5500443b8f9f91a3677928416e208cca72fd
diane <dianephan12@gmail.com>
2020-05-03 20:31:28 -0700

    added very helpful debugging statements. attempted login. gave up. w/e

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ var bodyParser = require('body-parser');
 const path = require('path');           //this helps the google maps api show up
 const db = require('./db');
 
-
-
-
 //for login session
 const redis = require('redis');
 const redisStore = require('connect-redis')(session);
 const client  = redis.createClient();
+var parseurl = require('parseurl')
 //////////////////////////////
 
 const app = express();
@@ -26,7 +24,7 @@ app.use(session({
     // create new redis store.
     store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
     saveUninitialized: true,
-    resave: true //originally F but unsure
+    resave: false //originally F but unsure
 }));
 
 // const router = express.Router();     //no idea what this does 
@@ -42,24 +40,25 @@ app.get('/admin',(req,res) => {
     }
     else {
         res.write('<h1>Please login first.</h1>');
-        res.end('<a href='+'/'+'>Login</a>');
+        res.end('<a href='+'/login'+'>Login</a>');
     }
 });
 
 app.get('/logout',(req,res) => {
-    const email = req.body.email
-    console.log("yo u trying to log out %s", email);
+    req.session.email = req.body.email
+    console.log("yo u trying to log out ", req.session.email);    
     // res.write(`<h1>Hello u tryna log out? </h1><br>`);
     req.session.destroy((err) => {
         if(err) {
             return console.log(err);
         }
-        res.end('<a href='+'/bye'+'>Logout</a>');
+        res.end('<a href = '+'/bye'+'>Logout</a>');
     });
 });
 
-
 app.get('/bye',(req,res) => {
+    req.session.email = req.body.email
+    console.log("peace out", req.session.email);    
     res.send("you have peaced out");
     // res.write(`<h1> you have peaced out </h1><br>`);
 });
@@ -88,8 +87,13 @@ app.post('/loginsession', async (req, res) => {
     const pass = req.body.password
 
     const myQuery = `SELECT EXISTS (SELECT 1 FROM users WHERE email = '${email}' AND  pass = '${pass}')`
+    const idQuery  = `SELECT * FROM users WHERE email = '${email}' AND  pass = '${pass}'`       //for the logged in user's info
     const result = await db.query(myQuery)
-    // await db.query(myQuery)
+    const idResult = await db.query(idQuery)
+    const currentID = idResult.rows[0].id
+    const username = idResult.rows[0].userid
+    
+    console.log(username, "'s id = ", currentID)
     console.log(result.rows[0].exists)
     if (result.rows[0].exists) {
         var htmlData = 'Hello:' + email + ' u successfully logged in';
