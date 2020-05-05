b15ba602e5a3890a75c023579dc024849ac1c858
diane <dianephan12@gmail.com>
2020-05-05 15:42:02 -0700

    added checkboxes the map

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const redisStore = require('connect-redis')(session);
 const client  = redis.createClient();
 var parseurl = require('parseurl')
 
+//url parsing
+const http = require('http');
+const url = require('url');
 //////////////////////////////
 
 const app = express();
@@ -24,7 +27,7 @@ app.use(session({
     secret: 'ssshhhhh',
     // create new redis store.
     store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
-    saveUninitialized: true,
+    saveUninitialized: false,
     resave: true //originally F but unsure
 }));
 
@@ -81,42 +84,85 @@ app.post('/register', async (req, res) => {
     res.send("Success boyo")
 })
 
+//trying a node tutorial 
+
+http.createServer(function (req, res) {
+    const queryObject = url.parse(req.url,true).query;
+    console.log(queryObject);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Feel free to add query parameters to the end of the url');
+  }).listen(5000);
+
 app.get('/login', async (req, res) => {
     // var sessionData = req.session;   //dont remember what this is
     const email = req.body.email
     const pass = req.body.password
+
+    //should i combine logindex with their personalized map 
     res.render('logindex.ejs')
 })
 
-
 ////// USER LOGS IN TO SEE WHAT THEY HAVE AND WHAT THEY DONT HAVE \\\\\\
 app.post('/loginsession', async (req, res) => {
-    const email = req.body.email
-    const pass = req.body.password
+    req.session.email = req.body.email
+    req.session.pass = req.body.password
 
-    const myQuery = `SELECT EXISTS (SELECT 1 FROM users WHERE email = '${email}' AND  pass = '${pass}')`
-    const idQuery  = `SELECT * FROM users WHERE email = '${email}' AND  pass = '${pass}'`       //for the logged in user's info
+    const myQuery = `SELECT EXISTS (SELECT 1 FROM users WHERE email = '${req.session.email}' AND  pass = '${req.session.pass}')`
+    const idQuery  = `SELECT * FROM users WHERE email = '${req.session.email}' AND  pass = '${req.session.pass}'`       //for the logged in user's info
     const result = await db.query(myQuery)
     const idResult = await db.query(idQuery)
     const currentID = idResult.rows[0].id
     const username = idResult.rows[0].userid
     
+    
     console.log(username, "'s id = ", currentID)
     console.log(result.rows[0].exists)
+
     if (result.rows[0].exists) {
-        var htmlData = 'Hello:' + email + ' u successfully logged in';
-        console.log("post received: %s %s", email, pass);
-        res.send(htmlData)
+        var htmlData = 'Hello:' + req.session.email + ' u successfully logged in';
+        console.log("post received: %s %s", req.session.email, req.session.pass);
+        // res.send(htmlData)
+        res.sendFile(path.join(__dirname + '/resources/html/usermap.html'));
+        // TO DO : NEED TO REDIRECT TO PERSONALIZED HTML PAGE 
+        // res.render('collectcoin.ejs')
     } else {
         res.send("not scucessful")
     }
 })
 
-
+//getting usermap
+app.get('/usermap', (req, res) => {
+    res.sendFile(path.join(__dirname + '/resources/html/usermap.html'));
+})
 
 app.get('/collect', (req, res) => {
     res.render('collectcoin.ejs')
 })
+
+app.post('/coininsert', async (req, res) => {
+    const machine = req.body.machine
+    const coin = req.body.coin
+    const email = req.body.email
+
+    const idQuery  = `SELECT * FROM users WHERE email = '${email}'`       //for the logged in user's info
+    const idResult = await db.query(idQuery)
+    const currentID = idResult.rows[0].id
+
+    const machineQuery = `SELECT * FROM coins LEFT JOIN machines ON machines.id = coins.fk_machine_id LEFT JOIN locations ON locations.fk_machine_id = machines.id WHERE machinename = '${machine}'`
+    const machineResult = await db.query(machineQuery)
+    const machineID = machineResult.rows[0].fk_machine_id
+    console.log(machineID, "is the machineid")
+    const coinQuery = `SELECT * FROM coins WHERE fk_machine_id = '${machineID}' AND coinname = '${coin}'`
+    const coinResult = await db.query(coinQuery)
+    const coinID = coinResult.rows[0].id
+    console.log(coinID, "is the coinid for ", coin)
+
+    // const myQuery = `SELECT insert_coin_entry('${userid}', '${email}', '${pass}');`
+    //// (fk_user INT, fk_coins INT, input_year INT) 
+    var htmlData = 'Hello:' + email + ' u want to insert' + coin + 'to' + machine;
+    res.send(htmlData)
+})
+
 
 
 const port = process.env.PORT || 3000;
