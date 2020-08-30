7eadebc8d90aa656e5052adf63e13e4ea3fe7c13
dIANE phan <dianephan12@gmail.com>
2020-08-29 17:47:51 -0700

    remove config.js from repo

--- config.js
+++
@@ -1,3 +0,0 @@
-var config = { // this should be const instead if you're using ES6 standards
-  apiKey : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBt2uGZOui5nESr2PwoRVJxP2OUqYA-gp8&callback=initMap'
-}
